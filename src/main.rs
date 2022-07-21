6543a90f8db7743095dff588ae2c34bb2aa3837d
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-07-21 14:35:53 +0900

    ㅁㅇㅇ

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+
+struct Config {
+    query: String,
+    filename: String,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
